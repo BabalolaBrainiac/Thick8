65a0bd59e268abb9c909f5dfc8b512d1dfb0d3ed
BabalolaBrainiac <74297323+BabalolaBrainiac@users.noreply.github.com>
2021-10-25 22:09:25 +0800

    Create login.jsx

---
+++ client/src/pages/navbar/login.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+const Login = () =>{
+  return (
+      <React.Fragment>
+          <div>
+        <h1 className = 'login-header'>Login</h1>
+        
+        </div>
+        <button className='login-btn'>
+           Login
+        </button>
+
+      </React.Fragment>
+    
+  );
+}
+export default Login;
