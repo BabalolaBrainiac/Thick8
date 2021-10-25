ded0bd7cf0225a087e6f1f92cadaa7c172417b20
BabalolaBrainiac <74297323+BabalolaBrainiac@users.noreply.github.com>
2021-10-25 22:09:08 +0800

    Create home.jsx

---
+++ client/src/pages/navbar/home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+const Home = () =>{
+  return (
+      <React.Fragment>
+          <div>
+        <h1 className = 'index-header'>Welcome</h1>
+        
+        </div>
+        
+
+      </React.Fragment>
+    
+  );
+}
+export default Home;
