c4dca8d91097230c0c9fb6e7e0560b73d702849a
BabalolaBrainiac <74297323+BabalolaBrainiac@users.noreply.github.com>
2021-10-25 22:08:22 +0800

    Update indexPage.jsx

--- client/src/pages/indexPage.jsx
+++ client/src/pages/indexPage.jsx
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import Navigation from '../components/navigation/navigation';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './navbar/home';
+import Signup from './navbar/signup';
+import Login from './navbar/login';
+import How from './navbar/how'
 
-
-
-class IndexPage extends React.Component {
-    render() { 
-        return <div>
-            <React.Fragment>
-                <Navigation/>
-            </React.Fragment>
-        </div>;
-    }
+function IndexNav() {
+    return (
+        <Router>
+            <Navigation/>
+            <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/how' exact component={How} />
+            <Route path='/signup' exact component={Signup} />
+            <Route path='/login' exact component={Login} />
+            </Switch>
+        </Router>
+    )
 }
- 
-export default IndexPage;
+
+
+export default IndexNav;  
