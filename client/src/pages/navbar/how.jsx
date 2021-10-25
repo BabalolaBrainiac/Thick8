458050e2d778a67df114e90eb10889166c93c6ff
BabalolaBrainiac <74297323+BabalolaBrainiac@users.noreply.github.com>
2021-10-25 22:09:18 +0800

    Create how.jsx

---
+++ client/src/pages/navbar/how.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+const How = () =>{
+  return (
+      <React.Fragment>
+          <div>
+        <h1 className = 'how-it-works'>How Tikketti Works</h1>
+        
+        </div>
+        <button className='join-waitlist'>
+            Join Our Waitlist
+        </button>
+
+      </React.Fragment>
+    
+  );
+}
+export default How;
