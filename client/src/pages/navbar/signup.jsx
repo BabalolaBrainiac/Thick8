49498e261e102a482ba23891b9ecb4cc98fa4a02
BabalolaBrainiac <74297323+BabalolaBrainiac@users.noreply.github.com>
2021-10-25 22:09:31 +0800

    Create signup.jsx

---
+++ client/src/pages/navbar/signup.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+const Signup = () =>{
+  return (
+      <React.Fragment>
+          <div>
+        <h1 className = 'signup-header'>Create an Account</h1>
+        
+        </div>
+        <button className='signup-btn'>
+           Signup
+        </button>
+
+      </React.Fragment>
+    
+  );
+}
+export default Signup;
